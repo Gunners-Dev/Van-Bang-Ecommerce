db36540c9e0d4cc9d59fdb3eeecd760cdfc3b960
Farhad-Sayed <sayedtheprogrammer@gmail.com>
2021-07-06 20:58:09 +0600

    various changes made to shop pages

--- dist/fontawesome.bd9656ef.js
+++ dist/fontawesome.bd9656ef.js
@@ -2662,7 +2662,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "12126" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "8278" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
